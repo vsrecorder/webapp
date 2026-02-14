657ced58cee99a860396a8df8a3ff897a3e08bf9
sosomasox <sosomasox@gmail.com>
2026-02-14 10:41:53 +0900

    update

--- src/app/components/organisms/Cityleague/CityleagueResultCard.tsx
+++ src/app/components/organisms/Cityleague/CityleagueResultCard.tsx
@@ -40,7 +40,7 @@ export default function CityleagueResultCard({ result }: Props) {
         }}
       >
         <Card shadow="sm" className="py-3">
-          <CardHeader className="pb-0 pt-0 px-3 flex-col items-start gap-0.5">
+          <CardHeader className="pb-0 pt-0 px-3">
             <div className="font-bold text-medium">
               {result.rank === 1
                 ? "優勝"
@@ -54,15 +54,16 @@ export default function CityleagueResultCard({ result }: Props) {
                         ? "ベスト16"
                         : ""}
             </div>
-            <div className="px-1 flex flex-col items-start gap-0.5">
+          </CardHeader>
+          <CardBody className="p-3 gap-3">
+            <div className="flex flex-col items-start gap-0.5">
               <p className="text-tiny">プレイヤー名: {result.player_name}</p>
               <p className="text-tiny">プレイヤーID: {result.player_id}</p>
               <p className="text-tiny">
                 デッキコード: {result.deck_code ? result.deck_code : "なし"}
               </p>
             </div>
-          </CardHeader>
-          <CardBody className="px-2 py-3">
+
             <div className="relative w-full aspect-2/1">
               {!imageLoaded && <Skeleton className="absolute inset-0 rounded-lg" />}
               {result.deck_code ? (
@@ -104,20 +105,22 @@ export default function CityleagueResultCard({ result }: Props) {
           {(onClose) => (
             <>
               <ModalHeader className="p-3 pb-0">
-                {result.rank === 1
-                  ? "優勝"
-                  : result.rank === 2
-                    ? "準優勝"
-                    : result.rank === 3
-                      ? "ベスト4"
-                      : result.rank === 5
-                        ? "ベスト8"
-                        : result.rank === 9
-                          ? "ベスト16"
-                          : ""}
+                <div className="font-bold text-medium">
+                  {result.rank === 1
+                    ? "優勝"
+                    : result.rank === 2
+                      ? "準優勝"
+                      : result.rank === 3
+                        ? "ベスト4"
+                        : result.rank === 5
+                          ? "ベスト8"
+                          : result.rank === 9
+                            ? "ベスト16"
+                            : ""}
+                </div>
               </ModalHeader>
-              <ModalBody className="p-3">
-                <div className="flex flex-col gap-0.5">
+              <ModalBody className="p-3 gap-3">
+                <div className="flex flex-col items-start gap-0.5">
                   <p className="text-tiny">プレイヤー名: {result.player_name}</p>
                   <p className="text-tiny">プレイヤーID: {result.player_id}</p>
                   <p className="text-tiny">

0fb5ad4f4cb23f5438e0eb4dbffc64a518f7ecef
sosomasox <sosomasox@gmail.com>
2026-02-15 23:21:29 +0900

    update

--- src/app/components/organisms/Matches.tsx
+++ src/app/components/organisms/Matches.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 import {
   Table,
@@ -10,6 +10,9 @@ import {
   TableRow,
   TableCell,
 } from "@heroui/react";
+
+import { Button } from "@heroui/react";
+import { Image } from "@heroui/react";
 
 import CreateMatchModal from "@app/components/organisms/CreateMatchModal";
 
@@ -47,6 +50,19 @@ export default function Matches({ record }: Props) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
+  const handler = useCallback(async () => {
+    try {
+      setLoading(true);
+      const data = await fetchMatches(record.id);
+      setMatches(data);
+    } catch (err) {
+      console.log(err);
+      setError("データの取得に失敗しました");
+    } finally {
+      setLoading(false);
+    }
+  }, [record.id]);
+
   useEffect(() => {
     if (!record) {
       setLoading(false);
@@ -80,50 +96,78 @@ export default function Matches({ record }: Props) {
   }
 
   if (!matches || matches.length === 0) {
-    return <CreateMatchModal record={record} />;
+    return <CreateMatchModal record={record} onCreated={handler} />;
   }
 
   return (
     <>
-      <Table
-        isStriped
-        hideHeader
-        //align="center"
-        aria-label="対戦結果"
-        classNames={{
-          th: "px-0 py-4",
-          td: "px-0 py-4",
-        }}
-      >
-        <TableHeader>
-          <TableColumn>勝ち/負け</TableColumn>
-          <TableColumn>先攻/後攻</TableColumn>
-          <TableColumn>相手のデッキ</TableColumn>
-        </TableHeader>
-        <TableBody>
-          {matches.map((match) => (
-            <TableRow key={match.id}>
-              <TableCell>{match.victory_flg === true ? "　⭕" : "　❌"}</TableCell>
-              <TableCell>
-                {match.default_victory_flg || match.default_defeat_flg ? (
-                  <>-</>
-                ) : (
-                  <>{match.games[0].go_first ? "先" : "後"}</>
-                )}
-              </TableCell>
-              <TableCell>
-                {match.default_victory_flg || match.default_defeat_flg ? (
-                  <>{match.default_victory_flg ? "不戦勝" : "不戦敗"}</>
-                ) : (
-                  <>{match.opponents_deck_info}</>
-                )}
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <div className="w-full overflow-y-auto">
+        <Table
+          removeWrapper
+          isStriped
+          hideHeader
+          radius="none"
+          aria-label="対戦結果"
+          classNames={{
+            th: "px-0 py-0",
+            td: "px-1 py-1",
+            tr: "border border-default-300",
+          }}
+        >
+          <TableHeader>
+            <TableColumn className="">情報</TableColumn>
+          </TableHeader>
+          <TableBody>
+            {matches.map((match) => (
+              <TableRow key={match.id}>
+                <TableCell>
+                  <Button variant="light" className="px-3 py-6 w-full">
+                    <div className="flex items-center gap-3 w-full">
+                      <div>{match.victory_flg === true ? "⭕" : "❌"}</div>
 
-      <CreateMatchModal record={record} />
+                      <div className="flex items-center font-bold">
+                        {match.default_victory_flg || match.default_defeat_flg ? (
+                          <div className="pl-1">-</div>
+                        ) : (
+                          <>{match.games[0].go_first ? "先" : "後"}</>
+                        )}
+                      </div>
+
+                      <div className="flex items-center gap-1.5">
+                        {match.default_victory_flg || match.default_defeat_flg ? (
+                          <>{match.default_victory_flg ? "不戦勝" : "不戦敗"}</>
+                        ) : (
+                          <>
+                            <div className="flex items-center gap-0 -translate-y-1 shrink-0">
+                              <Image
+                                alt="3_mega"
+                                src="/3_mega.png"
+                                className="w-11 h-11 object-cover scale-120 origin-bottom"
+                              />
+
+                              <Image
+                                alt="1017_teal"
+                                src="/1017_teal.png"
+                                className="w-11 h-11 object-cover scale-120 origin-bottom"
+                              />
+                            </div>
+                          </>
+                        )}
+
+                        <div className="font-bold truncate">
+                          {match.opponents_deck_info}
+                        </div>
+                      </div>
+                    </div>
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+
+      <CreateMatchModal record={record} onCreated={handler} />
     </>
   );
 }

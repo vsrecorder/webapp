b997800563e76c02c414bc5751ffc7b1251df2d4
sosomasox <sosomasox@gmail.com>
2026-02-23 00:55:38 +0900

    update

--- src/app/components/organisms/Cityleague/Skeleton/CityleagueResultSkeleton.tsx
+++ src/app/components/organisms/Cityleague/Skeleton/CityleagueResultSkeleton.tsx
@@ -7,7 +7,7 @@ export function CityleagueResultSkeleton() {
   return (
     <div className="">
       <Card className="pt-3 w-full">
-        <CardHeader className="pt-0 pb-2.5 px-3 flex-col items-start gap-1.5">
+        <CardHeader className="pt-1 pb-2.5 px-3 flex-col items-start gap-1.5">
           <small className="text-default-500">
             <Skeleton className="h-4 w-44" />
           </small>
@@ -20,7 +20,7 @@ export function CityleagueResultSkeleton() {
             <Skeleton className="h-5.5 w-50" />
           </div>
 
-          <div className="pt-1 flex flex-wrap items-start gap-1">
+          <div className="pt-0 flex flex-wrap items-start gap-1">
             <Skeleton className="h-5 w-12 rounded-2xl" />
             <Skeleton className="h-5 w-21 rounded-2xl" />
             <Skeleton className="h-5 w-24 rounded-2xl" />

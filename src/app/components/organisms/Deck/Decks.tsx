da277b98df69af4d30b7bd152291330196727dd0
sosomasox <sosomasox@gmail.com>
2026-06-19 02:47:36 +0900

    update

--- src/app/components/organisms/Deck/Decks.tsx
+++ src/app/components/organisms/Deck/Decks.tsx
@@ -2,11 +2,15 @@
 
 import { useEffect, useRef, useState, useCallback } from "react";
 
+import { Spinner } from "@heroui/spinner";
+import { Button } from "@heroui/react";
+
 import DeckCard from "@app/components/organisms/Deck/DeckCard";
+import { DeckCardSkeletons } from "@app/components/organisms/Deck/Skeleton/DeckCardSkeleton";
+
+import { LuCirclePlus } from "react-icons/lu";
 
 import { DeckType, DeckGetResponseType } from "@app/types/deck";
-
-import { DeckCardSkeletons } from "@app/components/organisms/Deck/Skeleton/DeckCardSkeleton";
 
 async function fetchDecks(isArchived: boolean, cursor: string) {
   try {
@@ -31,12 +35,11 @@ type Props = {
 };
 
 export default function Decks({ isArchived }: Props) {
-  const observerTarget = useRef<HTMLDivElement | null>(null);
-
   const [items, setItems] = useState<DeckType[]>([]);
   const [nextCursor, setNextCursor] = useState<string>("");
   const [isLoading, setIsLoading] = useState(false);
   const [hasMore, setHasMore] = useState(true);
+  const [isInitialLoaded, setIsInitialLoaded] = useState(false);
 
   const handleRemove = (id: string) => {
     setItems((prev) => prev.filter((d) => d.data.id !== id));
@@ -79,33 +82,23 @@ export default function Decks({ isArchived }: Props) {
       setHasMore(false);
     } finally {
       setIsLoading(false);
+      if (!isInitialLoaded) {
+        setIsInitialLoaded(true);
+      }
     }
   }, [isArchived, nextCursor, isLoading, hasMore]);
 
   useEffect(() => {
-    if (isLoading || !hasMore) return;
-
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          loadMore();
-        }
-      },
-      {
-        threshold: 0.5,
-      },
-    );
-
-    const target = observerTarget.current;
-    if (target) observer.observe(target);
-
-    return () => observer.disconnect();
-  }, [isLoading, hasMore, loadMore]);
+    if (isInitialLoaded) return;
+    loadMore();
+  }, [isInitialLoaded, loadMore]);
 
   return (
     <div className="flex flex-col items-center space-y-3 pb-3">
       {/* 空状態 */}
-      {!isLoading && !hasMore && items.length === 0 && <>デッキがありません</>}
+      {isInitialLoaded && !isLoading && !hasMore && items.length === 0 && (
+        <>デッキがありません</>
+      )}
 
       <div className="flex flex-col w-full gap-3">
         {items.map((deck) => (
@@ -120,8 +113,18 @@ export default function Decks({ isArchived }: Props) {
 
         {/* ローディング表示 */}
         {isLoading && <DeckCardSkeletons />}
+        {isInitialLoaded && isLoading && <Spinner size="lg" className="pt-0" />}
 
-        <div ref={observerTarget} className="h-1 w-full" />
+        {isInitialLoaded && !isLoading && hasMore && (
+          <Button size="sm" radius="full" onPress={loadMore}>
+            <div className="flex items-center gap-1">
+              <span className="text-xs">
+                <LuCirclePlus />
+              </span>
+              <span className="font-bold text-xs">更に読み込む</span>
+            </div>
+          </Button>
+        )}
       </div>
     </div>
   );
